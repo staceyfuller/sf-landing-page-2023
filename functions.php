1174efb2dd5e637e638fe9de9e0add234ef01f89
Stacey Fuller <75340324+staceyfuller@users.noreply.github.com>
2023-02-12 17:04:15 +1100

    added tracking code

--- functions.php
+++ functions.php
@@ -31,7 +31,7 @@ if (!function_exists('staceyfullerlanding_setup')) :
      * Note that this function is hooked into the after_setup_theme hook, which runs
      * before the init hook.
      */
-    function staceyfullertwentytwo_setup()
+    function staceyfullerlanding_setup()
     {
         // Add support for block styles.
         add_theme_support('wp-block-styles');
@@ -46,3 +46,35 @@ if (!function_exists('staceyfullerlanding_setup')) :
     }
 endif; // myfirsttheme_setup
 add_action('after_setup_theme', 'staceyfullerlanding_setup');
+
+
+function add_to_head()
+{
+    echo "<!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-5PTG5H7');
+    </script>
+    <!-- End Google Tag Manager -->";
+}
+add_action('wp_head', 'add_to_head');
+
+function add_to_body_top()
+{
+    echo ' <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5PTG5H7" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->';
+}
+add_action('wp_body_open', 'add_to_body_top');
